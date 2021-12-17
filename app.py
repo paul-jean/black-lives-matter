a39bc8c39dbd0166b8b6957ace3b1220cf3c60e3
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2021-12-16 19:53:05 -0800

    send async message using twilio message resource

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from tasks import schedule_response
 import os
 import redis
 
+TEST_CLIENT_PHONE = os.environ['TEST_CLIENT_PHONE']
+
 app = Flask(__name__)
 
 
@@ -13,6 +15,9 @@ app = Flask(__name__)
 def bot():
     # add webhook logic here and return a response
     incoming_msg = request.values.get('Body', '').lower()
+    print(request.values)
+    # phone_number = request.values.get('Number')
+    phone_number = '+17786867970'  # temp
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
@@ -24,7 +29,7 @@ def bot():
         msg.body('I only know about famous quotes and cats, sorry!')
     async_message = "... and 5 second delayed response!"
     r = schedule_response.apply_async(
-        args=(async_message,), countdown=1)
+        args=(async_message, TEST_CLIENT_PHONE), countdown=1)
     # r.get(on_message=on_raw_message, propagate=False)
     return str(resp)
 
