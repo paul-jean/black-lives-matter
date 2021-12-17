c96d7102c747552e932612b7db55afc8607998bf
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2021-12-16 20:03:30 -0800

    get the client phone from the incoming message metadata

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from tasks import schedule_response
 import os
 import redis
 
-TEST_CLIENT_PHONE = os.environ['TEST_CLIENT_PHONE']
-
 app = Flask(__name__)
 
 
@@ -15,9 +13,8 @@ app = Flask(__name__)
 def bot():
     # add webhook logic here and return a response
     incoming_msg = request.values.get('Body', '').lower()
+    client_phone = request.values.get('From')
     print(request.values)
-    # phone_number = request.values.get('Number')
-    phone_number = '+17786867970'  # temp
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
@@ -30,7 +27,7 @@ def bot():
     async_message = "... and 5 second delayed response!"
     async_delay = 60
     r = schedule_response.apply_async(
-        args=(async_message, TEST_CLIENT_PHONE), countdown=async_delay)
+        args=(async_message, client_phone), countdown=async_delay)
     # r.get(on_message=on_raw_message, propagate=False)
     return str(resp)
 
