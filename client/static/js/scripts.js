35d41680b8fd04832a78de49b76947b68b8a9a1a
PJ Letourneau <paul.jean.letourneau@gmail.com>
2022-06-18 22:42:07 -0700

    Add counter animation

--- client/static/js/scripts.js
+++ client/static/js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    $('div.main').text("hello world!");
+    $('div.main').text("0");
+    let counter = $('div.main');
+    const updateCount = () => {
+        const count = parseInt(counter.innerText);
+        const target = 100;
+        const increment = 1;
+    
+        if (count < target) {
+          counter.innerText = count + increment;
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      updateCount();
   });
