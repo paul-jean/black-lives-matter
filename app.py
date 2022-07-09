a4ad9172ef53c0c3bf46bb31996f7d900a92da2f
PJ Letourneau <paul.jean.letourneau@gmail.com>
2022-07-09 16:24:08 -0700

    Load csv data

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from flask import request
 from urllib.parse import urlparse
 
 from datetime import datetime
+import csv
 import os
-# from flask_sqlalchemy import SQLAlchemy, declarative_base
-# from sqlalchemy import create_engine
-# from flask_migrate import Migrate
+
+from flask_sqlalchemy import SQLAlchemy, Column, Integer, Date, String, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_migrate import Migrate
 
 
 app = Flask(__name__, static_folder='client/static')
@@ -23,18 +26,54 @@ if os.environ.get('FLASK_ENV') == 'development':
 else:
     app.config.from_object('config.ProdConfig')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-#     "DATABASE_URL").replace("postgres", "postgresql")
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL").replace("postgres", "postgresql")
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/index.html')  
 def serve_static_files_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
-# in seconds:
-HALF_HOUR = 60*30
-HOUR_AND_A_HALF = 60*90
+
+def csv_to_dict(file_name):
+    with open(file_name, newline='') as csvfile:
+        csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(csvfile.splitlines(), skipinitialspace=True)]
+        return csv_dicts
+
+class Black_Victim(Base):
+    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    __tablename__ = 'Black_Victim'
+    __table_args__ = {'sqlite_autoincrement': True}
+    #tell SQLAlchemy the name of column and its attributes:
+    id = Column(Integer, primary_key=True, nullable=False) 
+    name = Column(String)
+    birth_date = Column(Date)
+    death_date = Column(Date)
+    age = Column(Integer)
 
 if __name__ == '__main__':
+    #Create the session
+    session = sessionmaker()
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    session.configure(bind=engine)
+    s = session()
+
+    csv_dict = csv_to_dict('./static/data/data.csv')
+    try:
+        for i in csv_dict:
+            record = Black_Victim(**{
+                'name': i[0],
+                'birth_date' : datetime.strptime(i[1], '%b %d %Y').date(),
+                'death_date' : datetime.strptime(i[2], '%b %d %Y').date(),
+                'age': i[3]
+            })
+            s.add(record) #Add all the records
+
+        s.commit() #Attempt to commit all the records
+    except:
+        s.rollback() #Rollback the changes on error
+    finally:
+        s.close() #Close the connection
+
     app.run()
