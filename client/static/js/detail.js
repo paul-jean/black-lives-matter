a8c098e96554f2679f58e372d3a95b72a0c43849
PJ Letourneau <paul.jean.letourneau@gmail.com>
2022-09-25 22:43:42 -0700

    Add grid to separate years, months, days, etc

--- client/static/js/detail.js
+++ client/static/js/detail.js
@@ -8,28 +8,6 @@ $(document).ready(function() {
   const exhibit_endpoint = '/api/exhibit/' + encodeURIComponent(CURRENT_CITY);
 
   $.get(endpoint, function( victim ) {
-    console.log('ajax request to /detail = ')
-    console.log(victim)
-    victim_birth_str = moment(victim.birth_date).format("dddd, MMMM Do YYYY");
-    victim_death_str = moment(victim.death_date).format("dddd, MMMM Do YYYY");
-    $('<p>', {
-      text: `Born: ${victim_birth_str}`, 
-      id: 'birth'
-    }).appendTo(div);
-    $('<p>', {
-      text: `Died: ${victim_death_str}`, 
-      id: 'death'
-    }).appendTo(div);
-    let time_since_death_div = $('<p>', {
-      text: 0,
-      id: 'counter_since_death',
-      class: 'time_since_death'
-    }).appendTo(div)[0];
-    let time_dead_since_exhibit_div = $('<p>', {
-      text: 0,
-      id: 'counter_dead_since_exhibit',
-      class: 'time_dead_since_exhibit'
-    }).appendTo(div)[0];
     const death_seconds = Date.parse(victim.death_date);
 
     // https://stackoverflow.com/questions/26311489/obtain-difference-between-two-dates-in-years-months-days-in-javascript/26311490#26311490
@@ -49,20 +27,24 @@ $(document).ready(function() {
       m.add(-seconds, 'seconds');
       var ms = m.diff(d2, 'milliseconds');
     
-      return {years: years, months: months, days: days, hours: hours, minutes: minutes, seconds: seconds, ms: ms};
+      return {years: years, months: months, days: days, hours: hours, minutes: minutes, seconds: seconds, milliseconds: ms};
     }
 
     $.get(exhibit_endpoint, function(exhibit) {
       const exhibit_start_seconds = Date.parse(exhibit.start_date);
+      const row_div = $('container.time_since_death');
+
       const updateCounters = () => {
         const now_seconds = Date.now();
         const time_since_death_dict = time_diff_dict(now_seconds, death_seconds);
-        const time_since_death_str =  `${time_since_death_dict['years']} years, ${time_since_death_dict['months']} months, ${time_since_death_dict['days']} days, ${time_since_death_dict['hours']} hours, ${time_since_death_dict['minutes']} minutes, ${time_since_death_dict['seconds']} seconds, ${time_since_death_dict['ms']} ms`;
-        time_since_death_div.innerText = `Dead for: ${time_since_death_str}`;
+        time_since_death_dict.keys().map(function(k) {
+          row_div.find(k).innerText = time_since_death_dict[k];
+        });
 
         const time_dead_since_exhibit_dict = time_diff_dict(now_seconds, exhibit_start_seconds);
-        const time_dead_since_exhibit_str =  `${time_dead_since_exhibit_dict['years']} years, ${time_dead_since_exhibit_dict['months']} months, ${time_dead_since_exhibit_dict['days']} days, ${time_dead_since_exhibit_dict['hours']} hours, ${time_dead_since_exhibit_dict['minutes']} minutes, ${time_dead_since_exhibit_dict['seconds']} seconds, ${time_dead_since_exhibit_dict['ms']} ms`;
-        time_dead_since_exhibit_div.innerText = `Dead since exhibit: ${time_dead_since_exhibit_str}`;
+        time_dead_since_exhibit_dict.keys().map(function(k) {
+          row_div.find(k).innerText = time_dead_since_exhibit_dict[k];
+        });
 
         setTimeout(updateCounters, 1);
       };
